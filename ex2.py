9e5d3b66dd519cee80b29bdb018092f386507155
Jacob Postema <jpostema@vmware.com>
2017-06-22 16:53:45 -0400

    exercises 1-3  Please enter the commit message for your changes. Lines starting

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# A comment, this is so you can read your program later
+# Anything after the # is ignored by python
+
+print "I could have code like this." # and the cpmment after is ignored
+
+# 
+#
+
+print "This will run."
