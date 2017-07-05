c1544e2dee5096a0b740454a866c6cedf5df9feb
Jacob Postema <jpostema@vmware.com>
2017-07-05 09:20:37 -0400

    exercise 12

--- ex11.py
+++ ex11.py
@@ -5,4 +5,4 @@ height = raw_input()
 print "How much do you weight?",
 weight = raw_input()
 
-print "So you're %s old, %s tall, and %s heavy." % (age, height, weight)
+print "So, you're %s old, %s tall and %s heavy." % (age, height, weight)
