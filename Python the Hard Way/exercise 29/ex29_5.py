ce2836d0d5dca7cacb11abe5614e360ad5b094e0
Jacob Postema <deltakhan@users.noreply.github.com>
2018-04-21 14:33:27 -0500

    FInsihed exercise 29

---
+++ Python the Hard Way/exercise 29/ex29_5.py
@@ -0,0 +1,29 @@
+people = 18
+cats = 5
+dogs = 20
+
+if people < cats:
+    print "Too many cats! The world is doomed!"
+
+if people > cats:
+    print "Not many cats! The worls is saved!"
+
+if people < dogs:
+    print "The world is drooled on!"
+
+if people > dogs:
+    print "The world is dry!"
+
+
+
+dogs += 5
+
+if people >= dogs:
+    print "People are greater than or equal to dogs."
+
+if people <= dogs:
+    print "People are less that or equal to dogs."
+
+
+if people == dogs:
+    print "People are dogs."
