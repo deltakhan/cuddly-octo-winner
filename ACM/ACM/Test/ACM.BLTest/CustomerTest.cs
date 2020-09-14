a1db1603092b381700b0c4ec9201a6217e1d3735
Jacob Postema <jpostema@vmware.com>
2020-09-14 08:20:01 -0400

    testing Customer BL

---
+++ ACM/ACM/Test/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,61 @@
+using System;
+using ACM.BL;
+using Xunit;
+
+namespace ACM.BLTest
+{
+    public class CustomerTest
+    {
+        [Fact]
+        public void FullNameTestValid()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Bilbo",
+                LastName = "Baggins"
+            };
+            string expect = "Baggins, Bilbo";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.Equal(expect, actual);
+        }
+
+        [Fact]
+        public void FullNameFirstNameEmpty()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                LastName = "Baggins"
+            };
+            string expect = "Baggins";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.Equal(expect, actual);
+        }
+
+        [Fact]
+        public void FullNameLastNameEmpty()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Bilbo"
+            };
+            string expect = "Bilbo";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.Equal(expect, actual);
+        }
+    }
+}
